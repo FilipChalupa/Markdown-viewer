1f776a71bac07f7fad37e7684c6beb40c344a0ff
Filip Chalupa <chalupa.filip@gmail.com>
2022-07-11 23:52:54 +0200

    Add Material UI

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button'
+import { FunctionComponent } from 'react'
+
+export const App: FunctionComponent = () => {
+	return (
+		<>
+			Hello World! <Button variant="contained">Yes</Button>
+		</>
+	)
+}
