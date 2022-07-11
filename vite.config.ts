107d042aab890fe0d01f91b2e3cd598be7f6b982
Filip Chalupa <chalupa.filip@gmail.com>
2022-07-12 00:42:06 +0200

    Make PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,82 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	build: {
+		assetsInlineLimit: 0,
+	},
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Your Markdown Viewer',
+				short_name: 'MD viewer',
+				start_url: '.',
+				display: 'standalone',
+				theme_color: '#1976D2',
+				background_color: '#FFFFFF',
+				description: '@TODO',
+				categories: ['productivity', 'utilities'],
+				related_applications: [],
+				icons: [
+					{
+						src: 'icons/icon-48x48.png',
+						sizes: '48x48',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-72x72.png',
+						sizes: '72x72',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-96x96.png',
+						sizes: '96x96',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-128x128.png',
+						sizes: '128x128',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-144x144.png',
+						sizes: '144x144',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-152x152.png',
+						sizes: '152x152',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-384x384.png',
+						sizes: '384x384',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+					{
+						src: 'icons/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable any',
+					},
+				],
+			},
+		}),
+	],
 })
