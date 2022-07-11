4ba1b89a0e64c9e3e18ac86b781b8f922799da23
Filip Chalupa <chalupa.filip@gmail.com>
2022-07-12 00:06:00 +0200

    Add app bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,5 @@
+import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import { FunctionComponent } from 'react'
 import { Theme } from './Theme'
@@ -5,6 +7,23 @@ import { Theme } from './Theme'
 export const App: FunctionComponent = () => {
 	return (
 		<Theme>
+			<AppBar position="sticky">
+				<Toolbar>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+						Markdown viewer
+					</Typography>
+					<Button color="inherit">@todo</Button>
+				</Toolbar>
+			</AppBar>
 			Hello World!
 			<br />
 			<br />
